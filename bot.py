972e6779b6880fdba567e40bce378275746fd7cd
Senior Skete <jamiekarter02@gmail.com>
2026-01-21 19:12:11 +0000

    Fix indentation issue

--- bot.py
+++ bot.py
@@ -59,7 +59,6 @@ async def chests(ctx):
     await ctx.send(message)
     await ctx.message.delete()
     @bot.command()
-    
 @bot.command()
 async def remindme(ctx, time: str, *, reminder: str):
     await ctx.message.delete()
