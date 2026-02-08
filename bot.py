ee1e33c719c92757389858227001cc92459a0cb2
Senior Skete <jamiekarter02@gmail.com>
2026-02-08 18:01:10 +0000

    Removed old commands and added new setup tool

--- bot.py
+++ bot.py
@@ -1285,7 +1285,7 @@ reminder_group = app_commands.Group(
 @app_commands.describe(time="10m, 2h, 1d", reminder="Reminder text")
 async def reminder_set(interaction, time: str, reminder: str):
         unit = time[-1]
-    amount = time[:-1]
+        amount = time[:-1]
 
     if not amount.isdigit():
         return await interaction.response.send_message(
